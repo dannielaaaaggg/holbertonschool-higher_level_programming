4c726c217548712e08213f2ccd73c2e4aff53180
dannielaaaaggg <danielagomez.mendez09@gmail.com>
2026-01-29 08:19:02 -0600

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Lista todos los privilegios de los usuarios user_0d_1 y user_0d_2
+-- Script que lista los privilegios de los usuarios user_0d_1 y user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
