b01478c2e7d6bd8bdfa56417d94e444ff0974abe
dannielaaaaggg <danielagomez.mendez09@gmail.com>
2026-01-29 08:20:05 -0600

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,7 @@
--- Script que lista los privilegios de los usuarios user_0d_1 y user_0d_2
+-- Crea los usuarios si no existen para que el comando SHOW GRANTS no falle
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+-- Lista los privilegios de ambos usuarios
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
